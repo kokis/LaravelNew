f4bc1b4eff708c48754d6600ef24963c1acbbcd1
kokis <kokisru@gmail.com>
2016-05-26 01:56:33 +0300

    Initial commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>@yield('title')</title>
+        <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+
+        @yield('header')
+    </head>
+
+    <body>
+        @yield('content')
+
+        @yield('footer')
+    </body>
+</html>
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('title')Каталог товаров@stop
+
+@section('content')
+    Здесь будут товары
+@stop
