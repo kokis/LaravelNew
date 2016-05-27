1623c5c217b5314fe045010e7432cf6620ac131d
kokis <kokisru@gmail.com>
2016-05-27 18:10:38 +0300

    Categories and Product classes added

--- database/migrations/2016_05_26_151245_create_products_table.php
+++ database/migrations/2016_05_26_151245_create_products_table.php
@@ -14,7 +14,20 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('article');
+            $table->string('code');
             $table->string('title');
+            $table->text('description');
+            $table->string('image');
+            $table->tinyInteger('ageStart');
+            $table->tinyInteger('ageEnd');
+            $table->float('cost');
+            $table->integer('price');
+            $table->integer('priceRecommended');
+            $table->integer('weight');
+            $table->integer('width');
+            $table->integer('height');
+            $table->integer('length');
             $table->timestamps();
         });
     }
