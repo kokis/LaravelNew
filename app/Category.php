ca8c75e831b9b4faec110957ded3a7390ec0f188
kokis <kokisru@gmail.com>
2016-06-07 17:52:41 +0300

    Category - Category many to many relation

--- app/Category.php
+++ app/Category.php
@@ -6,9 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    public function belongs()
+    /**
+     * Вложенные категории
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function children()
     {
-        return $this->belongsToMany(Category::class, 'categories_relations' , 'parent_id', 'child_id');
+        return $this
+                ->belongsToMany(Category::class, 'categories_relations' , 'parent_id', 'child_id')
+                ->withTimestamps();
+    }
+
+    /**
+     * Родительские категории
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function parents()
+    {
+        return $this
+            ->belongsToMany(Category::class, 'categories_relations' , 'child_id', 'parent_id')
+            ->withTimestamps();
     }
 
     public function products()
