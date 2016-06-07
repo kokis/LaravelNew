4d7965ea89d5a3d186388398626094070d49fedb
kokis <kokisru@gmail.com>
2016-06-07 17:18:56 +0300

    Category - Product many to many relation

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function belongs()
+    {
+        return $this->belongsToMany(Category::class, 'categories_relations' , 'parent_id', 'child_id');
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
