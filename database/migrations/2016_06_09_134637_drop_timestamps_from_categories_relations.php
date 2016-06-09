5446a3f9a326f785a195c1704873d45f4e1fd2df
kokis <kokisru@gmail.com>
2016-06-09 16:50:10 +0300

    category_relations timestamps dropped

---
+++ database/migrations/2016_06_09_134637_drop_timestamps_from_categories_relations.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropTimestampsFromCategoriesRelations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories_relations', function (Blueprint $table) {
+            $table->dropColumn(['created_at', 'updated_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories_relations', function (Blueprint $table) {
+            $table->timestamps();
+        });
+    }
+}
